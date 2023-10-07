a337bd76b78ef5c218af635db770ca36b89b044e
Dev <devsapariya94@gmail.com>
2023-10-07 08:55:30 +0530

    connect the mongoDB

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
+
+with open('config.json') as  f:
+    config = json.load(f)
+    
+client = MongoClient(f"mongodb+srv://{config['mongo_username']}:{config['mongo_password']}@{config['mongo_host']}/")
+db_name= config['mongo_db']
+db = client.get_database(db_name)
+
+vendor_collection_name = "Vendor"
+user_collection_name = "User"
+
+vendor_collection = db.get_collection(vendor_collection_name)
+user_collection = db.get_collection(user_collection_name)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/vendor-registration', methods = ["GET", "POST"])
+def vendor_registration():
+    if request.method=="POST":
+        name=request.form.get('name')
+        email= request.form.get('email')
+        address = request.form.get('first_address')
+        mobile= request.form.get('tel')
+        companyNo = request.form.get('companyNo')
+        product = request.form.get('product')
+        print(name)
+        print(email)
+        print(address)
+        print(mobile)
+        print(companyNo)
+        print(product)
+        vendor_data = {
+            "name": name,
+            "email": email,
+            "address": address,
+            "mobile": mobile,
+            "companyNo": companyNo,
+            "productDetail": product
+        }
+        vendor_collection.insert_one(vendor_data)
+        return redirect("/")
+    return render_template('form.html')
+
+@app.route('/event')
+def event():
+    return render_template("event.html")
 if __name__ == '__main__':
     app.run(debug=True, host ="0.0.0.0")
